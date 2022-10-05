4e8f5ea4cb7d622dbc2fa6b1af76df0783a3a11a
AliakseiK1 <aliaksei.krasnahir@gmail.com>
2022-10-05 19:08:15 +0300

    Finishing config and adding hello_world application

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@4rj-)0+=d_9l$lo%f^lljr#4=d0n-n2#nj7yzau45-@l_^d(q'
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexeykrasnogir.pythonanywhere.com']
 
 
 # Application definition
